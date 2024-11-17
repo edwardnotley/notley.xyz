ed04ab3919a282ab6db5531859129741ef153306
n1mb0stratu5 <edwardnotley@gmail.com>
2024-11-17 19:25:57 +0000

    shifting dropdown to app

--- expenses/app.py
+++ expenses/app.py
@@ -5,22 +5,24 @@ app.secret_key = "your_secret_key"  # Needed if you use sessions
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    currencies = ["USD", "EUR", "GBP"]  # Predefined list of currencies
     if request.method == "POST":
         name = request.form.get("name")
         travel_purpose = request.form.get("travel_purpose")
         expenses = request.form.getlist("description")
         dates = request.form.getlist("date")
-        currencies = request.form.getlist("currency")
+        selected_currencies = request.form.getlist("currency")
         amounts = request.form.getlist("amount")
         attachments = request.files.getlist("attachment")
 
-        # You can process/save the data here or pass it to a new page
-        # Redirecting to printable for simplicity
-        return render_template("printable.html", 
-                               name=name, 
-                               travel_purpose=travel_purpose, 
-                               expenses=zip(expenses, dates, currencies, amounts, attachments))
-    return render_template("index.html")
+        # Process or save the data
+        return render_template(
+            "printable.html",
+            name=name,
+            travel_purpose=travel_purpose,
+            expenses=zip(expenses, dates, selected_currencies, amounts, attachments),
+        )
+    return render_template("index.html", currencies=currencies)
 
 if __name__ == "__main__":
     app.run(debug=True)
